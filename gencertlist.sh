ab599fe28cbe8a62785d7ff8d8b97a7af25f6169
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2017-03-03 18:11:53 +0100

    Add README

---
+++ gencertlist.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curd=`pwd`;
+CERTDIR=`grep CERTDIR caops.conf |cut -d '=' -f2|tr -d "'\""`
+echo -n >newcertlist.txt
+cd "$CERTDIR"; for i in `ls`; do subj=`openssl x509 -in $i -noout -subject|cut -d '=' -f2-`; echo "$subj:$i" >>$curd/newcertlist.txt; done; cd ..
